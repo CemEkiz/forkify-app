b9d39012e0dfd7785df5b0b990868b89910cbafc
Cem Ekiz <ekizcem.pro@gmail.com>
2022-09-04 20:39:04 +0200

    feat: update the search feature (render results)

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,13 @@
 import * as model from './model';
 import recipeView from './views/recipeView';
 import searchView from './views/searchView';
+import resultsView from './views/resultsView';
 import 'core-js';
 import 'regenerator-runtime';
+
+if (module.hot) {
+	module.hot.accept();
+}
 
 // API
 // https://forkify-api.herokuapp.com/v2
@@ -30,6 +35,9 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
 	try {
+		// Render a spinner animation during loading
+		resultsView.renderSpinner();
+
 		// 1. Get the user query from the value in the searchbar
 		const query = searchView.getQuery();
 		if (!query) return;
@@ -38,7 +46,8 @@ const controlSearchResults = async function () {
 		await model.loadSearchResults(query);
 
 		// 3. Render results
-		console.log(model.state.search.results);
+		// console.log(model.state.search.results);
+		resultsView.render(model.state.search.results);
 	} catch (err) {
 		console.log(err);
 	}
