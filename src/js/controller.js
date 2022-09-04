72a18c50e19d4bceb0f57a88321a45143de06f34
Cem Ekiz <ekizcem.pro@gmail.com>
2022-09-04 17:22:23 +0200

    feat: add search (without rendering)

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,6 @@
 import * as model from './model';
 import recipeView from './views/recipeView';
-
+import searchView from './views/searchView';
 import 'core-js';
 import 'regenerator-runtime';
 
@@ -29,8 +29,25 @@ const controlRecipes = async function () {
 	}
 };
 
+const controlSearchResults = async function () {
+	try {
+		// 1. Get the user query from the value in the searchbar
+		const query = searchView.getQuery();
+		if (!query) return;
+
+		// 2. Load the search results
+		await model.loadSearchResults(query);
+
+		// 3. Render results
+		console.log(model.state.search.results);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
 const init = function () {
 	recipeView.addHandlerRender(controlRecipes);
+	searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
